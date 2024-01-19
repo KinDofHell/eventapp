b8659abb9945f5cb68c7e28cf27faaf50a4e1db0
KinDofHell <ihordzhus24@gmail.com>
2024-01-19 20:31:44 +0200

    event details page + events card on home(without user checking)

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,10 +1,25 @@
 "use server";
 
-import { CreateEventParams } from "@/types";
+import { CreateEventParams, GetAllEventsParams } from "@/types";
 import { connectToDatabase } from "@/lib/mongodb/database";
 import { handleError } from "@/lib/utils";
 import User from "@/lib/mongodb/database/models/user.model";
 import Event from "@/lib/mongodb/database/models/event.model";
+import Category from "@/lib/mongodb/database/models/category.model";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({
+      path: "category",
+      model: Category,
+      select: "_id name",
+    });
+};
 
 export const createEvent = async ({
   event,
@@ -27,6 +42,50 @@ export const createEvent = async ({
     });
 
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (e) {
+    handleError(e);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (e) {
+    handleError(e);
+  }
+};
+
+export const getAllEvents = async ({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) => {
+  try {
+    await connectToDatabase();
+
+    const conditions = {};
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(0)
+      .limit(limit);
+
+    const events = await populateEvent(eventsQuery);
+    const eventsCount = await Event.countDocuments(conditions);
+
+    return {
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit),
+    };
   } catch (e) {
     handleError(e);
   }
